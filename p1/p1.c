fec20fb4e9a9cc29380b995645ba8fe8ec3abce0
fudgyone <nfaggler@gmail.com>
2018-01-03 16:37:56 -0800

    stack and queue functionality working

--- p1/p1.c
+++ p1/p1.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* preprocessor constants */
+#define N_ITEMS (100) /* arbitrary, for purposes of practice */
+
 /* struct Stack */
 typedef struct Stack {
 
 	/* elements of a stack */
 	int head; /* the current index of our stack accessible */
-	int items[]; /* all the items in our stack */
+	int items[N_ITEMS]; /* all the items in our stack */
 } Stack;
 
 /* our global stack */
@@ -60,7 +63,7 @@ typedef struct Queue {
 	/* elements of a Queue */
 	int first; /* next in line */
 	int last;  /* last in line */
-	int items[]; /* the line */
+	int items[N_ITEMS]; /* the line */
 } Queue;
 
 /* our global Queue */
@@ -70,15 +73,52 @@ Queue queue = {.first = 0, .last = 0};
 
 void enqueue(int value) {
 
+	printf("index: %d\n", queue.last); // why is this 30???
 
+	/* put our value in the Queue */
+	queue.items[queue.last] = value;
+
+	/* next spot in the Queue */
+	queue.last++;
 }
 
 int dequeue() {
 
+	/* grab the next item in the Queue */
+	int dequeued = queue.items[queue.first];
+
+	/* next! */
+	queue.first++;
+
+	/* if our first is back to our last, it's time to reset the queue */
+	if(queue.first == queue.last) {
+
+		queue.first = 0;
+		queue.last = queue.first;
+	}
+
+	/* return the dequeued item */
+	return dequeued;
+}
+
+void print_queue() {
+
+	int i;
+
+	if(queue.last > 0) {
+
+		for(i = queue.first; i < queue.last; i++) {
+
+			printf("Item %d: %d\n", i, queue.items[i]);
+		}
+	} else {
+
+		printf("Queue empty.\n");
+	}
 }
 
 /* function to interleave the stack with it's second half
- * may use one other queue, created in this function
+ * may use one other Queue, created above
  * 
  */
 void interleave() {
@@ -94,7 +134,7 @@ void requeue() {
 /* main driver function */
 int main(int argc, char *argv[]) {
 
-	/* test push and pop functionality */
+	/* test Stack functionality */
 
 	printf("Testing push\n");
 	push(10);
@@ -107,6 +147,23 @@ int main(int argc, char *argv[]) {
 	int x = pop();
 	printf("Item popped: %d\n", x);
 	print_stack();
+
+	/* test Queue functionality */
+
+	printf("Testing enqueue\n");
+	enqueue(10);
+	enqueue(20);
+	enqueue(30);
+	enqueue(40);
+	enqueue(69);
+	print_queue();
+	printf("Testing dequeue\n");
+	x = dequeue();
+	printf("Item dequeued: %d\n", x);
+	print_queue();
+
+	/* interleaving call */
+	// interleave();
 
 	return EXIT_SUCCESS;
 }
