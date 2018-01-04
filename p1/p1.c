098c8c3ef6c553353b0a83d98516c01e24a443f5
Fudgyone <thomaa1@unlv.nevada.edu>
2018-01-04 14:53:58 -0800

    started interleave algo

--- p1/p1.c
+++ p1/p1.c
@@ -47,6 +47,8 @@ void print_stack() {
 
 	if(stack.head > 0) {
 
+		printf("Printing stack:\n");
+
 		for(i = stack.head - 1; i >= 0; i--) {
 
 			printf("Item %d: %d\n", i, stack.items[i]);
@@ -72,8 +74,6 @@ Queue queue = {.first = 0, .last = 0};
 /* now the Queue operations... */
 
 void enqueue(int value) {
-
-	printf("index: %d\n", queue.last); // why is this 30???
 
 	/* put our value in the Queue */
 	queue.items[queue.last] = value;
@@ -107,6 +107,8 @@ void print_queue() {
 
 	if(queue.last > 0) {
 
+		printf("Printing queue:\n");
+
 		for(i = queue.first; i < queue.last; i++) {
 
 			printf("Item %d: %d\n", i, queue.items[i]);
@@ -117,25 +119,101 @@ void print_queue() {
 	}
 }
 
+/* abstracted procedure to re-enqueue until the desired
+ * value is in front of the queue */
+void requeue(int index) {
+
+	/* adjust the queue so that the index specified is now first */
+
+	/* !!! let the user know we're adjusting the queue !!! */
+	printf("Requeue!\n");
+	/* initial variables */
+	int end = queue.first + index - 1; /* how many to end at */
+	int i; /* iterator */
+
+	/* shift the queue */
+	for(i = queue.first; i <= end; i++) {
+
+		enqueue(dequeue());
+	}
+}
 /* function to interleave the stack with it's second half
  * may use one other Queue, created above
  * 
  */
 void interleave() {
 
-}
+	/* initial variables */
+	int i; /* iterator, used to store indices later */
 
-/* abstracted procedure to re-enqueue until the desired
- * value is in front of the queue */
-void requeue() {
+	/* !!! test print the current stack !!! */
+	print_stack();
 
+	/* pop everything off into the queue */
+	for(i = stack.head; i > 0; i--) {
+
+		enqueue(pop());
+	}
+
+	/* !!! test that the above code worked !!! */
+	print_stack();
+	print_queue();
+
+	/* now to grab the middle element */
+	i = ((queue.last - queue.first) / 2);
+	printf("requeues to middle index: %d\n", i);
+
+	/* requeue call to fix our queue to have the middle item first */
+	requeue(i);
+
+	/* !!! print the stack and the queue for checking !!! */
+	print_stack();
+	print_queue();
+
+	/* if the initial stack was an odd number of items,
+	 * we want to now dequeue two items at a time to
+	 * put back into the stack, requeue, and loop the
+	 * last two instructions until there are no more
+	 * elements
+	 *
+	 * otherwise, if the initial stack was an even number
+	 * of items, we want to dequeue one item to push, 
+	 * requeue, dequeue two items to push, and continue
+	 * the last two instructions until there are no more
+	 * elements
+	 *
+	 * both cases include the initial middle push
+	 */
+	printf("%d\n", queue.last - queue.first);
+
+	/* IMPORTANT: this for loop handles the odd case */
+	for(i = queue.last - queue.first; i > 0; i -= 2) {
+
+		if(i == 1) {
+
+			push(dequeue());
+			return;
+		}
+
+		/* dequeue 2 elements */
+		push(dequeue());
+		push(dequeue());
+
+		/* requeue */
+		requeue(2);
+
+		/* !!! print the stack and queue after every iteration */
+		print_stack();
+		print_queue();
+	}
 }
 
 /* main driver function */
 int main(int argc, char *argv[]) {
 
-	/* test Stack functionality */
+	/* !!! test Stack functionality !!! */
 
+	/*
 	printf("Testing push\n");
 	push(10);
 	push(20);
@@ -147,9 +225,11 @@ int main(int argc, char *argv[]) {
 	int x = pop();
 	printf("Item popped: %d\n", x);
 	print_stack();
+	*/
 
-	/* test Queue functionality */
+	/* !!! test Queue functionality !!! */
 
+	/*
 	printf("Testing enqueue\n");
 	enqueue(10);
 	enqueue(20);
@@ -161,9 +241,21 @@ int main(int argc, char *argv[]) {
 	x = dequeue();
 	printf("Item dequeued: %d\n", x);
 	print_queue();
+	*/
+
+	/* set up the stack for testing of interleaving */
+	int i; /* iterator */
+	for(i = 7; i > 0; i--) push(i); /* push 1-5 onto the stack */
+
+	/* queue is empty, as it should be initially */
 
 	/* interleaving call */
-	// interleave();
+	interleave();
+
+	/* print the finished product */
+	printf("Final results:\n");
+	print_stack();
+	print_queue();
 
 	return EXIT_SUCCESS;
 }
